f0fda5f67b733650e27d87c6bc4d57a11070bc95
Andres David Villarraga Piratoba <advillarragap@udistrital.edu.co>
2026-08-16 12:34:44 -0500

    feat: agrega metodo calcularsalud

--- src/gestionproyectos/Mago.java
+++ src/gestionproyectos/Mago.java
@@ -3,5 +3,8 @@ public class Mago{
      public void calcularpociones() {
     System.out.println("Calculando pociones del mago...");
 }
+public void calcularsalud() {
+    System.out.println("Calculando salud del mago...");
+}
 
 }
