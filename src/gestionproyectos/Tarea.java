387007250cc2316be159603f7e9f5fe3e6d7c867
Andres David Villarraga Piratoba <advillarragap@udistrital.edu.co>
2026-08-16 11:06:32 -0500

    feat: agrega constructor a la clase Tarea

--- src/gestionproyectos/Tarea.java
+++ src/gestionproyectos/Tarea.java
@@ -1,9 +1,13 @@
+package gestionproyectos;
+
 public class Tarea {
-    public Tarea(){
+    private String nombre;
+    private String descripcion;
+    private int duracion;
 
-
+    public Tarea(String nombre, String descripcion, int duracion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.duracion = duracion;
     }
-    
-
-
-    }
+}
